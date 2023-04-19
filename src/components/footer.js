19d31fe07c269c9c09f4b866934fd7e928a83cef
Christopher Baldock <christophermbaldock@gmail.com>
2023-04-19 14:22:57 -0500

    Adds Github, LinkedIn and Twitter navigational icon to the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { IconButton } from '@mui/material';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 function Footer() {
     return (
         <footer className='footer'>
-            <div>Some Stuff in my footer</div>
+            <IconButton href='https://github.com/CBaldock2'>
+                <GitHubIcon/>
+            </IconButton>
+            <IconButton href='https://www.linkedin.com/in/christopher-baldock/'>
+                <LinkedInIcon/>
+            </IconButton>
+            <IconButton href='twitter.com'>
+                <TwitterIcon/>
+            </IconButton>
         </footer>
     )
 }
