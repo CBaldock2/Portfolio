b0d07774bd606ffe06af62487e8e68266cb42890
Christopher Baldock <christophermbaldock@gmail.com>
2023-04-20 15:15:20 -0500

    adds styleing to the portfolio page

--- src/components/Project/ProjectCard.js
+++ src/components/Project/ProjectCard.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Card, Button, CardMedia, CardContent, Typography, CardActions, IconButton } from '@mui/material';
+import { Card, Button, CardMedia, CardContent, Typography, CardActions, IconButton, Chip } from '@mui/material';
 import GitHubIcon from '@material-ui/icons/GitHub';
+import '../../styles/style.css'
 
 function ProjectCard(props) {
     return (
-        <Card sx={{ maxWidth: 345 }}>
+        <Card sx={{ maxWidth: 550 }}>
             <CardMedia
-                sx={{ height: 140 }}
-                src={props.imgPath}
+                sx={{ height: 215 }}
+                image={props.imgPath}
                 alt='card-img'
             />
             <CardContent>
@@ -19,10 +20,18 @@ function ProjectCard(props) {
                 </Typography>
             </CardContent>
             <CardActions>
-                <Button size='small' href={props.deployed}>View</Button>
-                <IconButton size='small' href={props.ghLink}>
-                    <GitHubIcon />
-                </IconButton>
+                <div className='cardLinks'>
+                    <Button size='small' href={props.deployed}>View</Button>
+                    <IconButton size='small' href={props.ghLink}>
+                        <GitHubIcon />
+                    </IconButton>
+                </div>
+                <div className='chipsContainer'>
+                    {Array.isArray(props.skills) && props.skills.map((skill) => (
+                        <Chip key={skill} label={skill} className='projectSkill'
+                        />
+                    ))}
+                </div>
             </CardActions>
         </Card>
     )
